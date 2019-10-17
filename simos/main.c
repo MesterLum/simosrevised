58e7875ea15c901b72883692c140d425a02acc1d
Eduardo Cuauhtemoc Paez Palafox <mesterlum@hotmail.com>
2019-10-17 15:20:23 -0600

    Initial Commit

---
+++ simos/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simos
+//
+//  Created by DMI on 10/17/19.
+//  Copyright © 2019 DMI. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
